f3dedf8693da510db4ca67311da89d43d60f9e77
Vincent Moli <vincent.molinier@stu.ocsb.ca>
2024-10-31 11:15:18 -0400

    halloween spooky adventure

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 'use strict';
 
 /*** Event Listeners ***/
-
+document.getElementById("gamestart").addEventListener("click", game_start)
 
 
 /*** Functions ***/
@@ -26,4 +26,31 @@ function round(value, decimals) {
 // Get a random number from min to max
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function game_start() {
+    let name = prompt("A haunted tavern keeper asks for your name as you enter...🎃");
+    alert("Welcome " + name + " to my haunted tavern please enjoy your evening.");
+
+    let conversation = prompt("You glance over at the tavern keeper and he offers you a drink do you, drink from the Enchanted Goblet(1) or decline and go into another room(2).")
+
+    if (conversation == 1) {
+        enchant_goblet()
+    }
+
+    else if (conversation == 2) {
+        room_choice()
+    }
+}
+
+function enchant_goblet() {
+    alert ("You drank the goblet and for a short moment you were the most powerful person in the universe...... Unfortunately you couldn't withstand it and died instantly.")
+}
+
+
+function room_choice() {
+    alert("You politely decline the tarvern keep and make your way into another room.")
+    prompt("Which room do you go into bathroom(1) upstairs(2) back outside(3)")
 }
